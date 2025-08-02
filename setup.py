9478a1541dc235aba1033df181422308ed7a94b1
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2025-08-02 14:28:39 +0700

    update setup.py to 0.1.0b4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='vistock',
-    version='0.1.0-beta.3',
+    version='0.1.0-beta.4',
     install_requires=requirements,
     include_package_data=True,
     packages=find_packages(exclude=['tests', 'tests.*']),
